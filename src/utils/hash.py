6d2191892ebe43262f2a2a8e91d2380619bc3deb
dellius-alexander <dellius.alexander@gmail.com>
2023-05-05 16:24:00 -0400

    added function to check password

--- src/utils/hash.py
+++ src/utils/hash.py
@@ -21,3 +21,19 @@ def hash_password(password):
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
     log.info("Hashed password: %s" % hashed_password.decode("utf-8"))
     return hashed_password.decode("utf-8")
+
+
+# -----------------------------------------------------------------------------
+# Check a password
+def check_password(password, hashed_password) -> bool:
+    """
+    Check a password against an existing hash.
+
+    :param password: The password to check.
+    :param hashed_password: The existing hash.
+    :return: True if the password matches the hash. False otherwise.
+    """
+    # Hash the password with the salt
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
+
+# -----------------------------------------------------------------------------
